456a3568bbd9948f1af07fa648cd899b42aaa8ab
Andrew Vanweerden <ando.vanweerden@gmail.com>
2023-07-11 08:02:00 +0800

    Add preliminary files for day 9

---
+++ day9/main.py
@@ -0,0 +1,10 @@
+"""
+Another Matrix problem!
+1) Create Rope class and write method to determine behaviour of Tail when Head moves. It always moves adjacent to the Head after a move:
+    - when tail is in same row but different column, moves left or right 
+    - when tail is in different row but same column, moves up or down
+    - need to figure out diagonal movement
+2) Will need to track location of head and tail (properties on Rope class)
+3) Mark positions that Tail visits
+4) Count the number of positions the tail has visited
+"""
---
+++ day9/tests.py
@@ -0,0 +1,9 @@
+import sys
+import unittest
+from main import *
+
+class DayNineTests(unittest.TestCase):
+    file = "test_input.txt"
+
+if __name__ == '__main__':
+    unittest.main()
